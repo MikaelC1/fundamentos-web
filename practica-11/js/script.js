9fb2ba6cd54731d4d997349cdf31f599bbb24eb5
Mikael <amcp.negocios@gmail.com>
2022-11-21 18:26:11 -0600

    Practica 11

---
+++ practica-11/js/script.js
@@ -0,0 +1,36 @@
+const settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://superhero-search.p.rapidapi.com/api/heroes",
+	"method": "GET",
+	"headers": {
+		"X-RapidAPI-Key": "9f4d61d421msh6a92b1f9e40dfc3p1bd0dbjsna2611f447d30",
+		"X-RapidAPI-Host": "superhero-search.p.rapidapi.com"
+	},
+    dataType:'JSON',
+};
+
+$.ajax(settings).done(function (data) {
+    console.log(data)
+
+    data.forEach(element => {
+        let registro = '<div class="card col-3" style="width: 18rem;">' +
+        '<img src="'+element.images.sm+'" class="card-img-top" alt="...">' +
+        '<div class="card-body">' +
+          '<h5 class="card-title">'+ element.name +'</h5>' +
+          '<p class="card-text">'+element.biography.alignment+'</p>' +
+          '<a href="#" class="btn btn-primary">Go somewhere</a>' +
+        '</div>' +
+    '</div>'
+
+    $('#content-card').append(registro);
+    
+    })
+
+    
+});
+
+//image name biography
+
+
+
